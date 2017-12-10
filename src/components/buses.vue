bec658143b49df443a57c4d2dfc4430f6280cb3f
laxmanvijay <laxmanvijay24@gmail.com>
2017-12-10 16:12:53 +0530

    fetched from and to buses from backend

--- src/components/buses.vue
+++ src/components/buses.vue
@@ -1,16 +1,17 @@
 <template>
   <div class="w3-container">
-    <div class="w3-card-4">
+    <div class="w3-card-4 w3-margin" :key="data.busName" v-for="data in datas">
 
 <header class="w3-container w3-light-grey">
-  <h3></h3>
+  <h3>{{data.busName}}</h3>
 </header>
 
 <div class="w3-container">
-  <p>{{busName}}</p>
+  <p class="w3-text">Start point:{{data.startPoint}}</p>
+   <p class="w3-text">End point:{{data.endPoint}}</p>
   <hr>
   
-  <p></p>
+  <p class="w3-text">Seats: {{data.no_of_seats}}</p>
 </div>
 
 <button class="w3-button w3-block w3-dark-grey">Book Now</button>
@@ -25,14 +26,16 @@ export default {
     name:'footer',
     data(){
       return {
-        busName:[],
-        startPoint:[],
-        endPoint:[]
+       datas:[]
       }
     },
-    created:function(){
-      this.$http.get('http://localhost:8080/getBus',(data)=>{
-          this.busName=data
+    created(){
+      this.$http.post('http://localhost:8080/getBus',{
+        from:this.$route.query.from,
+        to:this.$route.query.to
+      }).then((data)=>{
+          this.datas=data.data
+          console.log(data.data)
       })
     }
 }
--- src/components/search.vue
+++ src/components/search.vue
@@ -3,11 +3,15 @@
    <form class="w3-container w3-card-4">
 
 <label class="w3-text w3-left w3-margin"><b>From</b></label>
-<input class="w3-input" type="text" ref="from" v-model="from">
+<input list="cities" class="w3-input" type="text" ref="from" v-model="from" @input="getCities($event)">
  
 <label class="w3-text w3-left w3-margin"><b>To</b></label>
-<input class="w3-input" type="text" ref="to" v-model="to">
-
+<input class="w3-input" type="text" ref="to" v-model="to" @input="getCities($event)">
+<datalist id="cities">
+  <div v-for="city in cities" :key="city">
+  <option :value="city"/>
+  </div>
+</datalist>
 <button class="w3-btn w3-teal w3-left w3-margin" @click="search">Search Buses!</button>
  
 </form>
@@ -21,12 +25,19 @@ export default {
     data(){
       return {
         from:'',
-        to:''
+        to:'',
+        cities:[]
       }
     },
     methods:{
       search(){
         this.$router.push({path:'/buses',query:{from:this.from,to:this.to}})
+      },
+      getCities(event){
+        console.log(event.data);
+        this.$http.get('http://gd.geobytes.com/AutoCompleteCity?callback=?&q='+event.data).then((res)=>{
+          this.cities=res;
+        })
       }
     }
 }
